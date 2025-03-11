edb840c5463c48c7022916bc9d20cabb6cf203d2
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-03-12 01:31:10 +0530

    added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
   return (
-    <div>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="flex justify-end p-4">
+        <ModeToggle />
+      </div>
+
       <AppRouter />
-    </div>
+    </ThemeProvider>
   );
 }
 
