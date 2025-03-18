98f177895255b7377fbb367c42f27c9cd7d68b8c
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-03-18 18:36:18 +0530

    added scrolling at home sidebar

--- src/routes/home/components/HomeSidebar.tsx
+++ src/routes/home/components/HomeSidebar.tsx
@@ -2,27 +2,31 @@ import { useUserStore } from "@/services/stores/user/userStore";
 import ChatList from "./ChatList";
 import GroupList from "./GroupList";
 import VoiceChatList from "./VoiceChatList";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const HomeSidebar = () => {
   const { userInfo } = useUserStore();
 
   return (
-    <aside className="w-80 bg-gray-100 dark:bg-gray-900 flex flex-col p-4 border-r rounded-bl-lg rounded-tl-lg dark:border-gray-700">
-      <div className="mb-4">
-        <h2 className="text-lg font-semibold">
-          {userInfo?.organizations[0].role.toUpperCase()}
-        </h2>
-      </div>
+    <ScrollArea>
+      <aside className="w-80 bg-gray-100 dark:bg-gray-900 flex flex-col p-4 border-r rounded-bl-lg rounded-tl-lg dark:border-gray-700">
+        <div className="mb-4">
+          <h2 className="text-lg font-semibold">
+            {userInfo?.organizations[0].role.toUpperCase()}
+          </h2>
+        </div>
 
-      {/* Voice Chats Section */}
-      <VoiceChatList />
+        {/* Voice Chats Section */}
 
-      {/* Groups Section */}
-      <GroupList />
+        <VoiceChatList />
 
-      {/* Direct Messages Section */}
-      <ChatList />
-    </aside>
+        {/* Groups Section */}
+        <GroupList />
+
+        {/* Direct Messages Section */}
+        <ChatList />
+      </aside>
+    </ScrollArea>
   );
 };
 
