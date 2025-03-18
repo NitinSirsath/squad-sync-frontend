8b81b78f8c64707034f8a78e4250f3ff83864e4b
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-03-18 14:33:37 +0530

    replaced login and register screens

--- src/routes/auth/login/LoginPage.tsx
+++ src/routes/auth/login/LoginPage.tsx
@@ -1,51 +1,53 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useLogin } from "@/hooks/auth/useAuth";
-import AuthCard from "../components/AuthCard";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useLogin } from "@/hooks/auth/useAuth";
+// import AuthCard from "../components/AuthCard";
+import { LoginForm } from "./LoginForm";
 
 const Login = () => {
-  const { mutate, status, error } = useLogin();
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  //   const { mutate, status, error } = useLogin();
+  //   const navigate = useNavigate();
+  //   const [email, setEmail] = useState("");
+  //   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    mutate(
-      { email, password },
-      {
-        onSuccess: () => {
-          navigate("/create-organization");
-        },
-      }
-    );
-  };
+  //   const handleLogin = () => {
+  //     mutate(
+  //       { email, password },
+  //       {
+  //         onSuccess: () => {
+  //           navigate("/create-organization");
+  //         },
+  //       }
+  //     );
+  //   };
 
   return (
-    <AuthCard
-      title="Sign in to Squad Sync"
-      subtitle="Use your work email to continue"
-      buttonText="Sign In"
-      onSubmit={handleLogin}
-      isLoading={status === "pending"}
-      inputs={[
-        {
-          type: "email",
-          placeholder: "Enter your email",
-          value: email,
-          onChange: (e) => setEmail(e.target.value),
-        },
-        {
-          type: "password",
-          placeholder: "Enter your password",
-          value: password,
-          onChange: (e) => setPassword(e.target.value),
-        },
-      ]}
-      error={error?.message}
-      footerText="Don't have an account?"
-      footerLinkText="Register"
-      onFooterClick={() => navigate("/register")}
-    />
+    // <AuthCard
+    //   title="Sign in to Squad Sync"
+    //   subtitle="Use your work email to continue"
+    //   buttonText="Sign In"
+    //   onSubmit={handleLogin}
+    //   isLoading={status === "pending"}
+    //   inputs={[
+    //     {
+    //       type: "email",
+    //       placeholder: "Enter your email",
+    //       value: email,
+    //       onChange: (e) => setEmail(e.target.value),
+    //     },
+    //     {
+    //       type: "password",
+    //       placeholder: "Enter your password",
+    //       value: password,
+    //       onChange: (e) => setPassword(e.target.value),
+    //     },
+    //   ]}
+    //   error={error?.message}
+    //   footerText="Don't have an account?"
+    //   footerLinkText="Register"
+    //   onFooterClick={() => navigate("/register")}
+    // />
+    <LoginForm />
   );
 };
 
--- src/routes/auth/register/RegisterPage.tsx
+++ src/routes/auth/register/RegisterPage.tsx
@@ -1,72 +1,74 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useRegister } from "@/hooks/auth/useAuth";
-import AuthCard from "../components/AuthCard";
-import { UserRegisterFormType } from "@/types/user.types";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useRegister } from "@/hooks/auth/useAuth";
+// import AuthCard from "../components/AuthCard";
+// import { UserRegisterFormType } from "@/types/user.types";
+import { RegisterForm } from "./RegisterForm";
 
 const Register = () => {
-  const { mutate, status, error } = useRegister();
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState<UserRegisterFormType>({
-    username: "",
-    email: "",
-    password: "",
-    firstName: "",
-    lastName: "",
-  });
+  //   const { mutate, status, error } = useRegister();
+  //   const navigate = useNavigate();
+  //   const [formData, setFormData] = useState<UserRegisterFormType>({
+  //     username: "",
+  //     email: "",
+  //     password: "",
+  //     firstName: "",
+  //     lastName: "",
+  //   });
 
-  const handleRegister = () => {
-    mutate(formData, { onSuccess: () => navigate("/login") });
-  };
+  //   const handleRegister = () => {
+  //     mutate(formData, { onSuccess: () => navigate("/login") });
+  //   };
 
   return (
-    <AuthCard
-      title="Create Your Account"
-      subtitle="Get started with Squad Sync"
-      buttonText="Register"
-      onSubmit={handleRegister}
-      isLoading={status === "pending"}
-      inputs={[
-        {
-          type: "text",
-          placeholder: "Username",
-          value: formData.username,
-          onChange: (e) =>
-            setFormData({ ...formData, username: e.target.value }),
-        },
-        {
-          type: "email",
-          placeholder: "Email",
-          value: formData.email,
-          onChange: (e) => setFormData({ ...formData, email: e.target.value }),
-        },
-        {
-          type: "password",
-          placeholder: "Password",
-          value: formData.password,
-          onChange: (e) =>
-            setFormData({ ...formData, password: e.target.value }),
-        },
-        {
-          type: "text",
-          placeholder: "First Name",
-          value: formData.firstName,
-          onChange: (e) =>
-            setFormData({ ...formData, firstName: e.target.value }),
-        },
-        {
-          type: "text",
-          placeholder: "Last Name",
-          value: formData.lastName,
-          onChange: (e) =>
-            setFormData({ ...formData, lastName: e.target.value }),
-        },
-      ]}
-      error={error?.message}
-      footerText="Already have an account?"
-      footerLinkText="Login"
-      onFooterClick={() => navigate("/login")}
-    />
+    // <AuthCard
+    //   title="Create Your Account"
+    //   subtitle="Get started with Squad Sync"
+    //   buttonText="Register"
+    //   onSubmit={handleRegister}
+    //   isLoading={status === "pending"}
+    //   inputs={[
+    //     {
+    //       type: "text",
+    //       placeholder: "Username",
+    //       value: formData.username,
+    //       onChange: (e) =>
+    //         setFormData({ ...formData, username: e.target.value }),
+    //     },
+    //     {
+    //       type: "email",
+    //       placeholder: "Email",
+    //       value: formData.email,
+    //       onChange: (e) => setFormData({ ...formData, email: e.target.value }),
+    //     },
+    //     {
+    //       type: "password",
+    //       placeholder: "Password",
+    //       value: formData.password,
+    //       onChange: (e) =>
+    //         setFormData({ ...formData, password: e.target.value }),
+    //     },
+    //     {
+    //       type: "text",
+    //       placeholder: "First Name",
+    //       value: formData.firstName,
+    //       onChange: (e) =>
+    //         setFormData({ ...formData, firstName: e.target.value }),
+    //     },
+    //     {
+    //       type: "text",
+    //       placeholder: "Last Name",
+    //       value: formData.lastName,
+    //       onChange: (e) =>
+    //         setFormData({ ...formData, lastName: e.target.value }),
+    //     },
+    //   ]}
+    //   error={error?.message}
+    //   footerText="Already have an account?"
+    //   footerLinkText="Login"
+    //   onFooterClick={() => navigate("/login")}
+    // />
+    <RegisterForm />
   );
 };
 
