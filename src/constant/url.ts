98bea11c8e443f84fe49adb64a95a1c80a0a23e1
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-03-19 13:33:13 +0530

    update url

--- src/constant/url.ts
+++ src/constant/url.ts
@@ -1,1 +1,2 @@
-export const BASE_URL_BACKEND = "http://localhost:8000";
+// export const BASE_URL_BACKEND = "http://localhost:8000";
+export const BASE_URL_BACKEND = "https://squad-sync-backend.onrender.com";
